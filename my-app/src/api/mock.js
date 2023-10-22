8b0dcdad24a084532ff149dd1c304b4e1b466306
lgh <573363068@qq.com>
2023-10-22 17:28:07 +0800

    用户管理页面---已完成

--- my-app/src/api/mock.js
+++ my-app/src/api/mock.js
@@ -4,8 +4,8 @@ import user from "./mockServeData/user";
 
 //定义mock请求拦截
 Mock.mock('/api/home/getData',homeApi.getStatisticalData)
-//用户列表数据
+//用户列表数据 
 Mock.mock('/api/user/add','post',user.createUser)
 Mock.mock('/api/user/edit','post',user.updateUser) 
 Mock.mock('/api/user/del','post',user.deleteUser)
-Mock.mock('/api/user/getUser',user.getUserList)
+Mock.mock(/api\/user\/getUser/,user.getUserList)
